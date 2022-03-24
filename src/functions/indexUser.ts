8a464f3814a7351bf3c8beb4b6768a3b492ea418
Vinicyus <vinicyus346@gmail.com>
2022-03-24 17:30:42 -0300

    feat: users crud

---
+++ src/functions/indexUser.ts
@@ -0,0 +1,15 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { document } from "../utils/dynamodbClient";
+
+export const handler: APIGatewayProxyHandler = async (_event) => {  
+  const users = await document.scan({
+    TableName: "users",     
+  }).promise();
+
+  console.log(users);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(users.Items)
+  }  
+}
